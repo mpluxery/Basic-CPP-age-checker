74611c5a9275e065eed0ce60ba41bdeb5a1cd70f
mpluxery <monica771827@gmail.com>
2025-06-01 01:56:34 +0000

    basic input output in cpp

---
+++ io.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+    int age = 0;
+    bool valid = false;
+    char null = '\0';
+
+    while (!valid)
+    {
+        std::cout << "Enter Age: ";
+
+        std::string line;
+        getline(std::cin, line);
+
+        std::stringstream is(line);
+
+
+        if (!(is >> age) || (is >> std::ws && is.get(null)) || age >= 100 || age <=0)
+            std::cout << "Dude be real!" << std::endl;
+        else
+            valid = true ; 
+    }
+
+    std::cout << "Youer " << age << " years old, seems legit!" << std::endl;
+    return 0;
+}
+
+
